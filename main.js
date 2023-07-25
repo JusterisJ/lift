4346b4715d5379f0ba106f5721bda33287457452
JusterisJ <justasjusteris@gmail.com>
2023-07-25 09:17:44 +0300

    pickup and spawning new passengers

--- main.js
+++ main.js
@@ -8,19 +8,37 @@ liftDiv.classList.add(`onFloor1`)
 buttonFloor3.addEventListener(`click`, e => {
     liftDiv.classList.remove(`onFloor1`, `onFloor2`)
     liftDiv.classList.add(`onFloor3`)
+    moveLift(e)
 })
 buttonFloor2.addEventListener(`click`, e => {
     liftDiv.classList.remove(`onFloor1`, `onFloor3`)
     liftDiv.classList.add(`onFloor2`)
+    moveLift(e)
 })
 buttonFloor1.addEventListener(`click`, e => {
     liftDiv.classList.remove(`onFloor2`,`onFloor3`)
     liftDiv.classList.add(`onFloor1`)
+    moveLift(e)
 })
 
 
 spawnPassenger()
 
+function moveLift(e) {
+    setTimeout(a =>{
+        checkForPassengers(e)
+    },1000)
+}
+function checkForPassengers(e) {
+    console.log(`checking for passengers`)
+    let currentFloor = `floor${e.target.innerText}`
+    console.log(document.querySelector(`.${currentFloor}`).firstElementChild)
+    if (document.querySelector(`.${currentFloor}`).firstElementChild) {
+        console.log(`has passenger`)
+        document.querySelector(`.passenger`).remove()
+        spawnPassenger()
+    }
+}
 
 function spawnPassenger() {
     let randomFloor = document.querySelector(`.floor${random(1,3)}`)
