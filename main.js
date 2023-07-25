5c75948562551f56eed318fdf41f2a475a15bfe9
JusterisJ <justasjusteris@gmail.com>
2023-07-25 08:43:14 +0300

    passenger spawning on random floor

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ let buttonFloor1 = document.querySelector(`.buttonFloor1`)
 let buttonFloor2 = document.querySelector(`.buttonFloor2`)
 let buttonFloor3 = document.querySelector(`.buttonFloor3`)
 
+
+liftDiv.classList.add(`onFloor1`)
 buttonFloor3.addEventListener(`click`, e => {
     liftDiv.classList.remove(`onFloor1`, `onFloor2`)
     liftDiv.classList.add(`onFloor3`)
@@ -16,4 +18,17 @@ buttonFloor1.addEventListener(`click`, e => {
     liftDiv.classList.add(`onFloor1`)
 })
 
-liftDiv.classList.add(`onFloor1`)
+
+spawnPassenger()
+
+
+function spawnPassenger() {
+    let randomFloor = document.querySelector(`.floor${random(1,3)}`)
+    let passenger = document.createElement(`div`)
+    passenger.classList.add(`passenger`)
+    randomFloor.appendChild(passenger)
+}
+
+function random(min, max) { // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
