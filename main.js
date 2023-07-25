0443d3d69a8d474054c1717d15461f10545df132
JusterisJ <justasjusteris@gmail.com>
2023-07-25 08:32:57 +0300

    moving lift with transitions

--- main.js
+++ main.js
@@ -2,8 +2,18 @@ let liftDiv = document.querySelector(`.lift`)
 let buttonFloor1 = document.querySelector(`.buttonFloor1`)
 let buttonFloor2 = document.querySelector(`.buttonFloor2`)
 let buttonFloor3 = document.querySelector(`.buttonFloor3`)
+
+buttonFloor3.addEventListener(`click`, e => {
+    liftDiv.classList.remove(`onFloor1`, `onFloor2`)
+    liftDiv.classList.add(`onFloor3`)
+})
 buttonFloor2.addEventListener(`click`, e => {
-    console.log(`a`)
-    liftDiv.classList.remove(`onFloor1`)
+    liftDiv.classList.remove(`onFloor1`, `onFloor3`)
     liftDiv.classList.add(`onFloor2`)
 })
+buttonFloor1.addEventListener(`click`, e => {
+    liftDiv.classList.remove(`onFloor2`,`onFloor3`)
+    liftDiv.classList.add(`onFloor1`)
+})
+
+liftDiv.classList.add(`onFloor1`)
